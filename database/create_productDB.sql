6bf8d8167caec714c0ff02ca1cbec7f97e2127db
silkycode <silkycode@users.noreply.github.com>
2024-03-11 15:39:30 -0700

    Added product category table and foreign key link to product table

--- database/create_productDB.sql
+++ database/create_productDB.sql
@@ -1,9 +1,15 @@
 .open products.db
 
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category_name TEXT
+);
+
 CREATE TABLE IF NOT EXISTS product (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     product_type TEXT NOT NULL,
     model_name TEXT NOT NULL,
     description TEXT NOT NULL,
     image BLOB
+    FOREIGN KEY (product_type) REFERENCES categories (category_id)
 );
