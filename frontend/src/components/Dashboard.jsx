5289b70695bd715b8c9170558f2809b9bca87a4a
Trev00 <treviourlee@gmail.com>
2024-03-11 14:27:34 -0700

    adding glucose log and it's DB but not tested yet and also deleted glucose.js so there won't be any confusion

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,37 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Container, Stack } from "@mui/material";
+import BackButton from '../assets/images/svgs/white-go-back-button.png';
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-
-  return (
-    <Container sx={{ mt: 4 }}>
-      <Stack spacing={2} direction="column">
-        <Button
-          variant="contained"
-          onClick={() => navigate("/products")}
-        >
-          View Products
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => navigate("/glucose")}
-        >
-          Glucose Logs
-        </Button>
-        {/* Uncomment below if you want to use the BackButton image as a back button
-        <Button
-          variant="contained"
-          startIcon={<img src={BackButton} alt="Go Back" />}
-          onClick={() => navigate(-1)}
-        >
-          Go Back
-        </Button>
-        */}
-      </Stack>
-    </Container>
-  );
+    return (
+        <p>dashboard here</p>
+    );
 };
 
 export default Dashboard;
