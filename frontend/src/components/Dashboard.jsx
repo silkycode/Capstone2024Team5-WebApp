50b4a245a447b5c4fff6ba896833337c7f570776
silkycode <silkycode@users.noreply.github.com>
2024-03-11 23:14:49 -0700

    Refactored dashboard with Vanessa's boxes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { styled } from '@mui/system';
 import { Route, Routes } from 'react-router-dom';
 import {
@@ -9,11 +10,20 @@ import {
   List,
   Divider,
   IconButton,
-  Container
+  Container,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography
 } from '@mui/material';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { mainListItems } from './DashboardNav';
+import {
+    AccountBox as AccountBoxIcon,
+    People as PeopleIcon,
+    Notifications as NotificationsIcon,
+    Assignment as AssignmentIcon,
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon
+  } from '@mui/icons-material';
 
 // Components
 import Appointments from './Appointments'
@@ -52,12 +62,48 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+const ClickableBox = ({ title, path }) => {
+    return (
+        <Link to={path} style={{ textDecoration: 'none' }}>
+            <Box
+                sx={{
+                width: '300px',
+                height: '200px',
+                backgroundColor: '#ff3856',
+                borderRadius: '4px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+                cursor: 'pointer',
+                transition: 'transform 0.3s',
+                '&:hover': {
+                    transform: 'translateY(-5px)',
+                },
+                }}
+            >
+                <Box sx={{ 
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100%',
+                    padding: '20px',
+                }}
+                >
+                <Typography variant="h5" component="div" color="white" fontWeight={'bold'} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)' }}>
+                    {title}
+                </Typography>
+                </Box>
+            </Box>
+        </Link>
+    );
+};
+
 export default function Dashboard() {
   const [open, setOpen] = React.useState(true);
 
   const toggleDrawer = () => {
     setOpen(!open);
   };
+
+
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -66,25 +112,76 @@ export default function Dashboard() {
             <Route path="/dashboard/glucose-logs" element={<GlucoseLogs />} />
             <Route path="/dashboard/appointments" element={<Appointments />} />
             <Route path="/dashboard/notifications" element={<Notifications />} />
+{/*         <Route path="/dashboard/product" element={<Profile />} />
+            <Route path="/dashboard/forms" element={<Profile />} /> */}
         </Routes>
         <CssBaseline />
         <Drawer variant="permanent" open={open}>
             <Toolbar
-            sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'flex-end',
-                px: [1],
-            }}
-            >
-            <IconButton onClick={toggleDrawer}
-            sx={{ color: 'white' }}>
-                {open ? <ChevronLeftIcon sx={{fontSize: '42px'}}/> : <ChevronRightIcon sx={{fontSize: '42px'}}/>}
-            </IconButton>
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    px: [1],
+                }}
+                >
+                <IconButton onClick={toggleDrawer}
+                sx={{ color: 'white' }}>
+                    {open ? <ChevronLeftIcon sx={{fontSize: '42px'}}/> : <ChevronRightIcon sx={{fontSize: '42px'}}/>}
+                </IconButton>
             </Toolbar>
             <Divider />
             <List component="nav">
-                {mainListItems}
+                <React.Fragment>
+                    <ListItemButton component={Link} to="/dashboard/profile">
+                    <ListItemIcon sx={{color: "white"}}>
+                        <AccountBoxIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={
+                        <Typography variant="h6" color="white" sx={{fontWeight: 'bold'}} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>
+                            Profile
+                        </Typography>
+                        }
+                    />
+                    </ListItemButton>
+                    <ListItemButton component={Link} to="/dashboard/glucose-logs">
+                    <ListItemIcon sx={{color: "white"}}>
+                        <AssignmentIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={
+                            <Typography variant="h6" color="white" sx={{fontWeight: 'bold'}} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>
+                            Glucose Logs
+                        </Typography>
+                        }
+                    />
+                    </ListItemButton>
+                    <ListItemButton component={Link} to="/dashboard/appointments">
+                    <ListItemIcon sx={{color: "white"}}>
+                        <PeopleIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={
+                        <Typography variant="h6" color="white" sx={{fontWeight: 'bold'}} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>
+                            Appointments
+                        </Typography>
+                        }
+                    />
+                    </ListItemButton>
+                    <ListItemButton component={Link} to="/dashboard/notifications">
+                    <ListItemIcon sx={{color: "white"}}>
+                        <NotificationsIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={
+                        <Typography variant="h6" color="white" sx={{fontWeight: 'bold'}} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>
+                            Notifications
+                        </Typography>
+                        }
+                    />
+                    </ListItemButton>
+                </React.Fragment>
             </List>
         </Drawer>
         <Box
@@ -95,7 +192,19 @@ export default function Dashboard() {
             overflow: 'auto',
             }}
         >
-            <Toolbar />
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <Typography variant="h4" gutterBottom component="div">
+                    Hello, USER!
+                </Typography>
+                <Box sx={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
+                    <ClickableBox title="Log Your Glucose" path="/dashboard/glucose-logs" />
+                    <ClickableBox title="Notifications" path="/dashboard/notifications"/>
+                    <ClickableBox title="Profile" path="/dashboard/profile"/>
+                    <ClickableBox title="Appointments"path="/dashboard/appointments" />
+                    <ClickableBox title="Product Info" path="/dashboard"/>
+                    <ClickableBox title="Profile" path="/dashboard"/>
+                </Box>
+            </Container>
         </Box>
     </Box>
   );
